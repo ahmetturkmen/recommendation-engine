2feb4d3eb16215a9ddc2be993d7b01d1b1893768
fsahin3458 <fsahin3458@gmail.com>
2017-08-11 15:51:13 +0300

    Symbol RegEx added

--- readFrom.js
+++ readFrom.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 let symbols = [], userIDs = [], logs = [];
+
 let readData = new Promise(function (resolve, reject) {
-    fs.readFile('/home/geek/Desktop/activity-logs.csv', 'utf8', (err, data) => {
+    fs.readFile('/home/fsahin3458/Masaüstü/activity-logs.csv', 'utf8', (err, data) => {
         if (err)
             reject();
         resolve(data);
@@ -10,41 +11,47 @@ let readData = new Promise(function (resolve, reject) {
 
 });
 
-readData
-    .then((data) => {
-        let temporaryData;
-        splitString(data, ';')
-
-        for (let i = 2; i < logs.length; i += 5) {
-            // userIDs.push(logs[i])
-            temporaryData = logs[i + 1].split('/[A-Z])\w+/g');
-            // symbols.push(temporaryData);
-            createObject(logs[i],temporaryData);
-        }
-    })
-    .catch((err) => console.log('Error happened : ' + err));
-
 function splitString(stringToSplit, separator) {
     let arrayOfStrings = stringToSplit.split(separator);
     arrayOfStrings.forEach(function (element) {
         logs.push(element)
     });
+    //console.log(logs);
     return logs;
 }
-function createObject(userID, symbolName) {
-    let object = new Object();
-    object.userID = userID;
-    object.symbolName = symbolName;
-    object.symbolValue ;
-    if (object.userID == userID) {
-        if (object.symbolName == userID.symbolName)
-           object.symbolValue++;
-    } else {
-        object.symbolName = symbolName;
-        object.symbolValue++;
-    }
-    console.log(object)
-}
+
+
+readData
+    .then((data) => {
+        let temporaryData;
+        splitString(data, ';')
+        
+        for (let i = 2; i < logs.length; i += 5) {
+            // userIDs.push(logs[i])
+            var u = /([A-Z]+)/g;
+            temporaryData = u.exec(logs[i + 1]);
+            console.log(temporaryData)
+            // symbols.push(temporaryData);
+            // createObject(logs[i],temporaryData);
+        }
+        
+    })
+    .catch((err) => console.log('Error happened : ' + err));
+
+// function createObject(userID, symbolName) {
+//     let object = new Object();
+//     object.userID = userID;
+//     object.symbolName = symbolName;
+//     object.symbolValue ;
+//     if (object.userID == userID) {
+//         if (object.symbolName == userID.symbolName)
+//            object.symbolValue++;
+//     } else {
+//         object.symbolName = symbolName;
+//         object.symbolValue++;
+//     }
+//     console.log(object)
+// }
 // let logData = '2017-08-09T13:51:36.502Z;MATRIKS;12951;/?symbol=GARAN&period=1min&start=2015-07-17&end=2015-07-17;veli-bar-data-api-node;bar-data-api';
 
 // let semiColon = ';';
