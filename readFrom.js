46cb59a472ad8df47273d4988dbce1e1615079d5
ahmetturkmen <f.ahmet.turkmen@icloud.com>
2017-08-14 12:11:34 +0300

    Issues Fixed; Working

--- readFrom.js
+++ readFrom.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-let userIDs = [], logs = [];
-let regEx = /([A-Z]+)/g; // To get symbols of the users 
-let bankLogs,filteredUsers,temporarySymbols=[],symbols=[];
-let objectOfUsers={};  let frequenceCounter={};
-let splitterOne=[],splitterTwo=[],temporaryVariable=' ';
+const isUpperCase = require('is-upper-case');
+let userIDs = [],logs=[];
+let objectOfUsers = {}; 
+let bankLogs, filteredUsers, temporarySymbols = [];
+
 
 let readData = new Promise(function (resolve, reject) {
     fs.readFile('/home/geek/Desktop/activity-logs.csv', 'utf8', (err, data) => {
@@ -15,6 +15,47 @@ let readData = new Promise(function (resolve, reject) {
 
 });
 
+readData
+    .then((data) => {
+        bankLogs = splitString(data, ';')
+    })
+    .then(() => {
+        for (let index = 2; index < bankLogs.length; index += 5) {
+            userIDs.push(bankLogs[index]);
+            temporarySymbols.push(bankLogs[index].concat(bankLogs[index + 1]));
+        }
+    })
+    .then(() => {
+        filteredUsers = userIDs.filter((value, index) => {
+            return userIDs.indexOf(value) === index
+        });
+    })
+    .then(() => {
+        filteredUsers.forEach(function (element) {
+            objectOfUsers[element] = {}
+        });
+    })
+    .then(() => {
+        createObject(temporarySymbols, 0)
+
+    })
+    .then(() => {
+        createObject(temporarySymbols, 1)
+    })
+    .then(() => {
+        createObject(temporarySymbols, 2);
+    })
+    .then(() => {
+        createObject(temporarySymbols, 3)
+    })
+    .then(() => {
+        console.log(objectOfUsers)
+
+    })
+
+    .catch((err) => console.log('Error happened : ' + err));
+
+
 function splitString(stringToSplit, separator) {
     let arrayOfStrings = stringToSplit.split(separator);
     arrayOfStrings.forEach(function (element) {
@@ -24,49 +65,20 @@ function splitString(stringToSplit, separator) {
     return logs;
 }
 
-
-readData
-    .then((data) => {
-        bankLogs = splitString(data, ';');
-    })
-    .then(()=>{
-       for (let index = 2; index < bankLogs.length; index += 5){ 
-                userIDs.push(bankLogs[index]);
-                temporarySymbols.push(bankLogs[index+1]);    
+function createObject(temporarySymbols, indexOfFilteredUsers) {
+    let counter, tempSlicedValue, tempSlicedValue2;
+    temporarySymbols.forEach(function (element) {
+        tempSlicedValue = element.slice(0, element.indexOf('/'));
+        tempSlicedValue2 = element.slice(tempSlicedValue.length + 9, element.indexOf('&'))
+        if (tempSlicedValue === filteredUsers[indexOfFilteredUsers] && isUpperCase(tempSlicedValue2) && tempSlicedValue2 !== '') {
+            if (objectOfUsers[filteredUsers[indexOfFilteredUsers]][tempSlicedValue2] === undefined) {
+                counter = 1;
+                objectOfUsers[filteredUsers[indexOfFilteredUsers]][tempSlicedValue2] = counter;
+            }
+            else if (objectOfUsers[filteredUsers[indexOfFilteredUsers]][tempSlicedValue2] !== undefined)
+                counter++;
+            objectOfUsers[filteredUsers[indexOfFilteredUsers]][tempSlicedValue2] = counter;
         }
+
     })
-    .then(()=>{
-        temporarySymbols.forEach((element)=> {
-            temporaryVariable+=element;
-        });
-       
-    })
-    .then(()=>{
-        splitterOne=temporaryVariable.split('=')
-        splitterOne.forEach(function(element) {
-            splitterTwo.push(element.split('&'));
-        });
-    })
-    .then(()=>{
-            console.log(splitterTwo)
-    })
-   /*  .then(()=>{
-        for (let i = 0; i < symbols.length; i++) {
-            let num = symbols[i];
-            frequenceCounter[num] = frequenceCounter[num] ? frequenceCounter[num] + 1 : 1;
-          }
-    }) */
-    .then(() => {
-      filteredUsers=userIDs.filter((value, index)=> {
-            return userIDs.indexOf(value) === index
-        });
-    })
-    .then(() => {
-        filteredUsers.forEach(function(element) {
-            objectOfUsers[element]={}   
-        });
-    })
-    .then(()=>{
-        console.log(objectOfUsers)
-    })
-    .catch((err) => console.log('Error happened : ' + err));
+}
