946bf906bf790ab661a1e0a1abd014e0b7cc47de
ahmetturkmen <f.ahmet.turkmen@icloud.com>
2017-08-15 14:24:42 +0300

    global variables reduced

--- readFrom.js
+++ readFrom.js
@@ -1,43 +1,42 @@
 const fs = require('fs');
 const isUpperCase = require('is-upper-case');
-let bankLogs = [], objectOfUsers = {}, objectKeys = [];
-let filteredUsers = [], temporarySymbols = [];
+let temporarySymbols = [];
 let readData = new Promise(function (resolve, reject) {
     fs.readFile('/home/geek/Desktop/activity-logs.csv', 'utf8', (err, data) => {
         if (err)
-            reject();
-        resolve(data);
+            reject(err);
 
+        let obj = {
+            data: data,
+            bankLogs: [],
+            objectOfUsers: {},
+            objectKeys: []
+        };
+        resolve(obj);
     });
 
 });
-readData
-.then(data => {
-    bankLogs = splitString(data, ';')
-    for (let index = 2; index < bankLogs.length; index += 5) {
-        objectOfUsers[bankLogs[index]] = {}
-        temporarySymbols.push(bankLogs[index].concat(bankLogs[index + 1]));
+
+
+readData.then(obj => {
+    obj.bankLogs = splitString(obj.data, ';')
+    for (let index = 2; index < obj.bankLogs.length; index += 5) {
+        obj.objectOfUsers[obj.bankLogs[index]] = {}
+        temporarySymbols.push(obj.bankLogs[index].concat(obj.bankLogs[index + 1]));
     }
-    Object.keys(objectOfUsers).forEach(function (element) {
-        objectKeys.push(element)
+    Object.keys(obj.objectOfUsers).forEach(function (element) {
+        obj.objectKeys.push(element)
     });
-    for (let index = 0; index < objectKeys.length; index++)
-        createObject(index);
-    console.log(objectOfUsers)
+    for (let index = 0; index < obj.objectKeys.length; index++)
+        createObject(index, obj.objectOfUsers, obj.objectKeys);
+    console.log(obj.objectOfUsers)
 })
     .catch((err) => console.log('Error happened : ' + err));
 
 
-function splitString(stringToSplit, separator) {
-    let logs = [];
-    let arrayOfStrings = stringToSplit.split(separator);
-    arrayOfStrings.forEach(function (element) {
-        logs.push(element)
-    });
-    return logs;
-}
 
-function createObject(indexOfUsers) {
+
+function createObject(indexOfUsers, objectOfUsers, objectKeys) {
     let counter, tempSlicedValue, tempSlicedValue2;
     temporarySymbols.forEach(function (element) {
         tempSlicedValue = element.slice(0, element.indexOf('/'));
@@ -55,7 +54,14 @@ function createObject(indexOfUsers) {
 
     })
 }
-
+function splitString(stringToSplit, separator) {
+    let logs = [];
+    let arrayOfStrings = stringToSplit.split(separator);
+    arrayOfStrings.forEach(function (element) {
+        logs.push(element)
+    });
+    return logs;
+}
 
 module.exports = {
     splitString,
